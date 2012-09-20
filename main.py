8f8543db277cc54e7fae6d5816f15af8c71b78ff
beef <met898@tamu.edu>
2012-09-20 10:36:45 -0500

    MainWindow Hello World

---
+++ main.py
@@ -0,0 +1,56 @@
+'''
+Tanks - Working Title
+First 2D Fall 2012 Game for TAGD Members
+
+'''
+
+import pygame
+import os
+
+SCREEN_SIZE=[800,600]
+
+class MainWindow:
+    def __init__(self):
+        print 'Initiated main window'
+        
+        # Define the colors we will use in RGB format
+        black=[  0,  0,  0]
+        white=[255,255,255]
+        
+        pygame.init()
+        screen=pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption("Tanks") #FIXME working title
+        
+        # Initialize timer for MainWindow
+        clock=pygame.time.Clock()
+        
+        font=pygame.font.Font(None,25)
+        # Render the text. "True" means anti-aliased text. 
+        # Black is the color. This creates an image of the 
+        # letters, but does not put it on the screen
+        text=font.render("Tanks - hello world",True,black)
+        
+        mainExit=False
+        while mainExit==False:
+            # This limits the while loop to a max of 45 times per second.
+            # Leave this out and we will use all CPU we can.
+            clock.tick(45)
+            
+            for event in pygame.event.get(): # User did something
+                # If user clicked close or hit escape key
+                if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                    mainExit=True # Flag that we are done so we exit this loop
+                # FIXME Add menu system here
+            # Put the image of the text on the screen at 250x250
+            screen.fill(white)
+            screen.blit(text, [250,250])
+            
+            # Go ahead and update the screen with what we've drawn.
+            # This MUST happen after all the other drawing commands.
+            pygame.display.flip()
+            
+if __name__ == '__main__':
+    print 'Working Directory: ', os.getcwd()
+    MainWindow()
+    print 'Exited MainWindow'
+
