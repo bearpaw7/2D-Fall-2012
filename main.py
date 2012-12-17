34c9fe5fa140fb6b66c4365951dea9dcd0358d39
beef <met898@tamu.edu>
2012-12-17 15:36:23 -0600

    Rebirth

--- main.py
+++ main.py
@@ -6,32 +6,36 @@ First 2D Fall 2012 Game for TAGD Members
 
 import pygame
 import os
+from tank import Tank
 
-SCREEN_SIZE=[800,600]
+SCREEN_SIZE = [800,600]
 
 class MainWindow:
     def __init__(self):
         print 'Initiated main window'
         
         # Define the colors we will use in RGB format
-        black=[  0,  0,  0]
-        white=[255,255,255]
+        black = [  0,  0,  0]
+        white = [255,255,255]
         
         pygame.init()
-        screen=pygame.display.set_mode(SCREEN_SIZE)
-        pygame.display.set_caption("Tanks") #FIXME working title
+        screen = pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption("Tanks Title") #FIXME working title
         
         # Initialize timer for MainWindow
-        clock=pygame.time.Clock()
+        clock = pygame.time.Clock()
         
-        font=pygame.font.Font(None,25)
+        font = pygame.font.Font(None, 25)
         # Render the text. "True" means anti-aliased text. 
         # Black is the color. This creates an image of the 
         # letters, but does not put it on the screen
-        text=font.render("Tanks - hello world",True,black)
+        text = font.render("Tanks - hello world", True, black)
         
-        mainExit=False
-        while mainExit==False:
+        tankPosition = [100, 100]
+        tank = Tank(tankPosition, "red")
+
+        mainExit = False
+        while mainExit == False:
             # This limits the while loop to a max of 45 times per second.
             # Leave this out and we will use all CPU we can.
             clock.tick(45)
@@ -39,11 +43,17 @@ class MainWindow:
             for event in pygame.event.get(): # User did something
                 # If user clicked close or hit escape key
                 if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                    mainExit=True # Flag that we are done so we exit this loop
+                    mainExit = True # Flag that we are done so we exit this loop
                 # FIXME Add menu system here
             # Put the image of the text on the screen at 250x250
             screen.fill(white)
             screen.blit(text, [250,250])
+            
+            # update the tank position
+            tankPosition = [ (tankPosition[0] + 1), (tankPosition[1] + 1) ]
+            tank.move(tankPosition)
+            # paint the tank
+            screen.blit(tank.image, tank.rect)
             
             # Go ahead and update the screen with what we've drawn.
             # This MUST happen after all the other drawing commands.
---
+++ tank.py
@@ -0,0 +1,48 @@
+'''
+Tanks - Working Title
+First 2D Fall 2012 Game for TAGD Members
+
+'''
+
+import pygame
+import os
+from math import pi
+
+class Tank(pygame.sprite.Sprite):
+    # Singleton tank images
+    RedTankImage = None
+    BlueTankImage = None
+    ImageScale = [100,100]
+
+    def __init__(self, startPosition, joinTeam):
+        pygame.sprite.Sprite.__init__(self) #initialize parent class
+
+        if joinTeam == 'red':
+            if Tank.RedTankImage == None:
+                Tank.RedTankImage = pygame.image.load( os.path.join('images', 'red_tank.png')).convert()
+                Tank.RedTankImage = pygame.transform.scale(Tank.RedTankImage, Tank.ImageScale)
+            self.image = Tank.RedTankImage
+        elif joinTeam == 'blue':
+            if Tank.BlueTankImage == None:
+                Tank.BlueTankImage = pygame.image.load( os.path.join('images', 'blue_tank.png')).convert()
+                Tank.BlueTankImage = pygame.transform.scale(Tank.BlueTankImage, Tank.ImageScale)
+            self.image = Tank.BlueTankImage
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = startPosition
+        self.team = joinTeam
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def move(self, newPosition):
+        self.rect = newPosition
+
+if __name__ == '__main__':
+    print '\'tank.py\' is not the correct startup script'
+    print 'Attempting to execute main.py...'
+    try:
+        execfile('main.py')
+    finally:
+        print 'Exited tank.py'
+
