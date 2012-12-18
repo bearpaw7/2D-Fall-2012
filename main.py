a1ee52e7b8ff38e013d5398e20e86c470ad8a5e5
Wally Tung <wallykai@gmail.com>
2012-12-18 14:11:06 -0600

    add cross-branch files

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 '''
-   
 Tanks - Working Title
 First 2D Fall 2012 Game for TAGD Members
 
@@ -7,31 +6,70 @@ First 2D Fall 2012 Game for TAGD Members
 
 import pygame
 import os
-import math 
+import math
+import time
 from sabot import Sabot 
+from tank import Tank
 
-SCREEN_SIZE=[800,600]
+SCREEN_SIZE = [800,600]
 
 class MainWindow:
+    sprites = pygame.sprite.Group()
+    
+    def userInput(self, event, tankPosition):
+        if event.type == pygame.KEYUP and event.key == pygame.K_LEFT:
+            self.dx = 0
+        elif event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
+            self.dx = 0
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            self.dx = -1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            self.dx = 1
+        if event.type == pygame.KEYUP and event.key == pygame.K_UP:
+            self.dy = 0
+        elif event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+            self.dy = 0
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            self.dy = 1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            self.dy = -1
+        if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+            self.dz = 0
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            self.dz = 1
+    
+    def userAttack(self):
+        if self.dz == 1:
+            if (time.time() - self.dzTime) > 1:
+                self.dzTime = time.time()
+                self.sprites.add(Sabot(self.localTank.rect[0], self.localTank.rect[1], math.pi))
+    
     def __init__(self):
         print 'Initiated main window'
         
         # Define the colors we will use in RGB format
-        black=[  0,  0,  0]
-        white=[255,255,255]
-        
+        black = [  0,  0,  0]
+        white = [255,255,255]
+        self.dx = 0  # x travel
+        self.dy = 0  # y travel
+        self.dz = 0  # shot firing
+        self.dzTime = 0 # shot last fired
+        self.speed = 3 # tank speed
         pygame.init()
-        screen=pygame.display.set_mode(SCREEN_SIZE)
-        pygame.display.set_caption("Tanks") #FIXME working title
+        screen = pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption("Tanks Title") #FIXME working title
         
         # Initialize timer for MainWindow
-        clock=pygame.time.Clock()
+        clock = pygame.time.Clock()
         
-        font=pygame.font.Font(None,25)
-        # Render the text. "True" means anti-aliased text. 
-        # Black is the color. This creates an image of the 
+        font = pygame.font.Font(None, 25)
+        # Render the text. "True" means anti-aliased text.
+        # Black is the color. This creates an image of the
         # letters, but does not put it on the screen
-        text=font.render("Tanks - hello world",True,black)
+        text = font.render("Tanks - hello world", True, black)
+        
+        tankPosition = [100, 100]
+        self.localTank = Tank(tankPosition, "red")
         
         testSabot = Sabot( 200,200, math.pi)
         
@@ -39,23 +77,41 @@ class MainWindow:
         while mainExit==False:
             # This limits the while loop to a max of 45 times per second.
             # Leave this out and we will use all CPU we can.
-            
             clock.tick(45)
             
             for event in pygame.event.get(): # User did something
                 # If user clicked close or hit escape key
                 if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                    mainExit=True # Flag that we are done so we exit this loop
+                    mainExit = True # Flag that we are done so we exit this loop
                 # FIXME Add menu system here
+                self.userInput(event, tankPosition)
             # Put the image of the text on the screen at 250x250
             screen.fill(white)
             screen.blit(text, [250,250])
             
+            # update the tank position
+#            tankPosition = [tankPosition[0] + (self.dx * self.speed), tankPosition[1] + (self.dy * self.speed)]
+#            self.localTank.moveTo(tankPosition)
+            if self.dy > 0:
+                self.localTank.moveForward()
+            elif self.dy < 0:
+                self.localTank.moveReverse()
+            if self.dx > 0:
+                self.localTank.rotateLeft()
+            elif self.dx < 0:
+                self.localTank.rotateRight()
+            # paint the tank
+            screen.blit(self.localTank.image, self.localTank.rect)
+            
             testSabot.draw(screen)
+            self.userAttack()
+            for shot in self.sprites.sprites():
+                shot.draw(screen)
             # Go ahead and update the screen with what we've drawn.
             # This MUST happen after all the other drawing commands.
             pygame.display.flip()
-            
+        
+    
 if __name__ == '__main__':
     print 'Working Directory: ', os.getcwd()
     MainWindow()
---
+++ tank.py
@@ -0,0 +1,82 @@
+'''
+Tanks - Working Title
+First 2D Fall 2012 Game for TAGD Members
+
+'''
+
+import os, sys
+import pygame
+from pygame.locals import *
+from math import pi, radians, cos, sin
+
+if not pygame.font: print 'Warning, fonts disabled'
+if not pygame.mixer: print 'Warning, sound disabled'
+
+class Tank(pygame.sprite.Sprite):
+    # Singleton tank images
+    RedTankImage = None
+    BlueTankImage = None
+    ImageScale = [50, 50]
+    DegreesRotated = 5
+    MoveRadius = 5
+
+    def __init__(self, startPosition, joinTeam):
+        pygame.sprite.Sprite.__init__(self) #initialize parent class
+
+        if joinTeam == 'red':
+            if Tank.RedTankImage == None:
+                Tank.RedTankImage = pygame.image.load( os.path.join('images', 'red_tank.png')).convert_alpha()
+                Tank.RedTankImage = pygame.transform.scale(Tank.RedTankImage, Tank.ImageScale)
+            self.image = Tank.RedTankImage
+        elif joinTeam == 'blue':
+            if Tank.BlueTankImage == None:
+                Tank.BlueTankImage = pygame.image.load( os.path.join('images', 'blue_tank.png')).convert_alpha()
+                Tank.BlueTankImage = pygame.transform.scale(Tank.BlueTankImage, Tank.ImageScale)
+            self.image = Tank.BlueTankImage
+
+        self.rect = self.image.get_rect()
+        self.rect.center = startPosition
+        self.team = joinTeam
+        self.facing = 0 # degrees
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def moveForward(self):
+        self.rect.center = [
+                            self.rect.center[0] - Tank.MoveRadius * sin( -radians(self.facing) ) , # x position
+                            self.rect.center[1] - Tank.MoveRadius * cos( radians(self.facing) )   # y position
+                            ]
+        
+    def moveReverse(self):
+        self.rect.center = [
+                            self.rect.center[0] + Tank.MoveRadius * sin( -radians(self.facing) ) , # x position
+                            self.rect.center[1] + Tank.MoveRadius * cos( radians(self.facing) )   # y position
+                            ]
+        
+    def moveTo(self, newPosition):
+        self.rect.center = newPosition
+    
+    def rotateLeft(self):
+        self.facing += Tank.DegreesRotated
+        self.rotateTo(self.facing)
+    
+    def rotateRight(self):
+        self.facing -= Tank.DegreesRotated
+        self.rotateTo(self.facing)
+        
+    def rotateTo(self, degrees):
+        # rotate a Surface, maintaining position
+        oldCenter = self.rect.center
+        self.image = pygame.transform.rotate( Tank.RedTankImage, -degrees )
+        self.rect = self.image.get_rect(center = oldCenter)
+        print "angle", self.facing, ", center", self.rect.center
+
+if __name__ == '__main__':
+    print '\'tank.py\' is not the correct startup script'
+    print 'Attempting to execute main.py...'
+    try:
+        execfile('main.py')
+    finally:
+        print 'Exited tank.py'
+
