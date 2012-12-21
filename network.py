b34cdc80929d631d8b57fb8f30b890c69c5f5b8f
beef <met898@tamu.edu>
2012-12-21 15:09:41 -0600

    server listening port

--- network.py
+++ network.py
@@ -6,16 +6,15 @@ client/server
 
 import socket as socketLib
 
-SERVER_PORT = 2013
-CLIENT_PORT = 2014
+PORT = 2013
 BACKLOG = 10 # tcp connection backlog
 
 class Network():
     def __init__(self, isServer):
         self.server = isServer
+        self.netSocket = socketLib.socket(socketLib.AF_INET, socketLib.SOCK_STREAM)
         if isServer == True:
-            self.netSocket = socketLib.socket(socketLib.AF_INET, socketLib.SOCK_STREAM)
-            self.netSocket.bind( ('0.0.0.0', SERVER_PORT) )
+            self.netSocket.bind( ('0.0.0.0', PORT) )
             self.netSocket.listen(BACKLOG)
     
 if __name__ == '__main__':
